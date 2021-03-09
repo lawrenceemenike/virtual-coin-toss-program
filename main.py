b3de05354476cc9c2437d5cde020fd23eb6fae6f
Lawrence Emenike <clenmenikev@gmail.com>
2021-03-09 06:49:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+random = random.randint(0, 1)
+
+if random == 1:
+  print('Heads')
+else:
+  print('Tails')
+
+
+
+
+
+
+
+
+
